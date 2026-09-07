4e05866427e57e207233f4e7eca64e8053aef758
vin <49336820+Draculabo@users.noreply.github.com>
2026-09-08 02:43:20 +0800

    refactor: consolidate component server public API imports

--- packages/component-gen-server/src/index.ts
+++ packages/component-gen-server/src/index.ts
@@ -6,11 +6,32 @@
  * (./standalone) wires the same routes + dsh-auth + dsh-artifacts + app bundle
  * into one node:http server.
  */
-export {
+import {
   createComponentGenHandler,
   type ComponentGenHandler,
   type ComponentGenHandlerDeps,
 } from './routes.js'
+import {
+  COMPONENT_GEN_ROUTE_PREFIX,
+  MAX_IMAGE_BYTES,
+  type ComponentGenConfig,
+  type ComponentGenPage,
+  type HistoryEntry,
+  type HistoryPage,
+  type HistoryPatch,
+  type HistoryQuery,
+  type JobEvent,
+  type JobInput,
+  type JobKind,
+  type JobState,
+  type StartJobRequest,
+} from './types.js'
+
+export {
+  createComponentGenHandler,
+  type ComponentGenHandler,
+  type ComponentGenHandlerDeps,
+}
 export { JobStore, runGeneration, type JobMeta, type RunOutcome } from './jobs.js'
 export { HistoryStore, parseDataUrl, newHistoryId, newImageId } from './history.js'
 export type { ComponentGenBackend, GenerationExec } from './backend.js'
@@ -28,9 +49,7 @@ export {
   type JobKind,
   type JobState,
   type StartJobRequest,
-} from './types.js'
-import { createComponentGenHandler, type ComponentGenHandlerDeps } from './routes.js'
-import { COMPONENT_GEN_ROUTE_PREFIX } from './types.js'
+}
 
 export interface ComponentGenWebRoute {
   kind: 'prefix'
